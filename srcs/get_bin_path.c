cae021b96922eed15a2cc0111656615a0158b025
Morgane FRISBY <mfrisby@e1r9p9.42.fr>
2017-11-22 16:47:05 +0100

    UPD norme

--- srcs/get_bin_path.c
+++ srcs/get_bin_path.c
@@ -1,70 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_bin_path.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfrisby <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/22 16:45:26 by mfrisby           #+#    #+#             */
+/*   Updated: 2017/11/22 16:46:31 by mfrisby          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/ft_script.h"
 
-static int  count_path(char *ret)
+static int			count_path(char *ret)
 {
-    int     i;
-    int     count;
+	int				i;
+	int				count;
 
-    i = 0;
-    if (!ret)
-        return (0);
-    count = 1;
-    while (ret[i])
-    {
-        if (ret[i] == ':')
-            count++;
-        i++;
-    }
-    return (count);
+	i = 0;
+	if (!ret)
+		return (0);
+	count = 1;
+	while (ret[i])
+	{
+		if (ret[i] == ':')
+			count++;
+		i++;
+	}
+	return (count);
 }
 
-static int         ret_copy(char *ret, int c, t_env *env)
+static int			ret_copy(char *ret, int c, t_env *env)
 {
-    int i;
-    int y;
-    int e;
+	int i;
+	int y;
+	int e;
 
-    i = 0;
-    y = 0;
-    while (ret[i])
-    {
-        if (ret[i] == ':')
-            break ;
-        env->path[c][i] = ret[i];
-        i++;
-    }
-    env->path[c][i] = '/';
-    e = i + 1;
-    i++;
-    while (env->command[0][y])
-    {
-        env->path[c][e] = env->command[0][y];
-        y++;
-        e++;
-    }
-    return (i);
+	i = 0;
+	y = 0;
+	while (ret[i])
+	{
+		if (ret[i] == ':')
+			break ;
+		env->path[c][i] = ret[i];
+		i++;
+	}
+	env->path[c][i] = '/';
+	e = i + 1;
+	i++;
+	while (env->command[0][y])
+	{
+		env->path[c][e] = env->command[0][y];
+		y++;
+		e++;
+	}
+	return (i);
 }
 
-static void         add_path(t_env *env, char *ret, int count)
+static void			add_path(t_env *env, char *ret, int count)
 {
-    int i;
-    int c;
+	int i;
+	int c;
 
-    c = 0;
-    i = 0;
-    while (ret[i] && count >= c)
-    {
-        env->path[c] = ft_mmap(1024);
-        i += ret_copy(ret + i, c, env);
-        c++;
-    }
+	c = 0;
+	i = 0;
+	while (ret[i] && count >= c)
+	{
+		env->path[c] = ft_mmap(1024);
+		i += ret_copy(ret + i, c, env);
+		c++;
+	}
 }
 
-void	get_bin_path(t_env *env, char **envp)
+void				get_bin_path(t_env *env, char **envp)
 {
-    int		i;
-    int     count;
-    char	*ret;
+	int				i;
+	int				count;
+	char			*ret;
 
 	i = 0;
 	ret = NULL;
@@ -73,13 +85,13 @@ void	get_bin_path(t_env *env, char **envp)
 		if ((ret = ft_strstr(envp[i], "PATH")) != NULL)
 		{
 			while (*ret != '=')
-                ret++;
-            ret++;
-            count = count_path(ret);
-            env->path = ft_mmap(sizeof(char*) * count + 1);
-            add_path(env, ret, count);
-            env->command[count + 1] = NULL;
-            break ;
+				ret++;
+			ret++;
+			count = count_path(ret);
+			env->path = ft_mmap(sizeof(char*) * count + 1);
+			add_path(env, ret, count);
+			env->command[count + 1] = NULL;
+			break ;
 		}
 		i++;
 	}
