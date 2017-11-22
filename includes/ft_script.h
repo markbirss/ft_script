29cdd46c09e0e7add1dfd80920fe1eadde6d380f
Morgane FRISBY <mfrisby@e1r9p9.42.fr>
2017-11-22 15:18:10 +0100

    UPD norme

--- includes/ft_script.h
+++ includes/ft_script.h
@@ -6,7 +6,7 @@
 /*   By: mfrisby <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 13:57:28 by mfrisby           #+#    #+#             */
-/*   Updated: 2017/11/17 13:59:17 by mfrisby          ###   ########.fr       */
+/*   Updated: 2017/11/22 15:16:15 by mfrisby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ typedef struct		s_env {
 }					t_env;
 
 void				ft_script(int fd_file, t_env *env);
-int    				 open_file(t_env *env);
-int					get_params(int ac, char **av, t_env **env);
+int					open_file(t_env *env);
+int					get_params(int ac, char **av, t_env *env);
 void				*ft_mmap(int size);
 char				*ft_mmap_strdup(const char *s1);
 int					create_pty(int *fd_master, int *fd_slave);
@@ -46,7 +46,7 @@ int					ft_posix_openpt(int flags);
 char				*ft_ptsname(int fd);
 int					ft_grantpt(int fd);
 int					ft_unlockpt(int fd);
-void        		set_raw_mode(t_env *env);
+void				set_raw_mode(t_env *env);
 int					ft_tcsetattr(int fd, struct termios *t);
 int					ft_tcgetattr(int fd, struct termios *t);
 
